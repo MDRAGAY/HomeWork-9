45f6480e2402b6d1c82f8c1ec1e879084c8edb1c
mdrag <m.dragay@yandex.ru>
2023-01-25 11:33:16 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
     }
-}
+
+    private static void task1() {
+        System.out.println("Задача 1");
+        int[] payments = generateRandomArray();
+        System.out.println(Arrays.toString(payments));
+        int sum = 0;
+        for (int i = 0; i < payments.length; i++) {
+            sum = sum + payments[i];
+
+        }
+        System.out.println("Сумма трат за месяц составила" + sum + "рублей");
+    }
+
+        // Объявить метод «сгенерироватьМассив»
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+    }
+        
+
+
