1ec3b37c6dd9ce4b7cca921769da46ec518bccf6
mdrag <m.dragay@yandex.ru>
2023-01-25 12:27:02 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
 
@@ -18,16 +19,37 @@ public class Main {
         System.out.println("Сумма трат за месяц составила" + sum + "рублей");
     }
 
-        // Объявить метод «сгенерироватьМассив»
-        public static int[] generateRandomArray () {
-            java.util.Random random = new java.util.Random();
-            int[] arr = new int[30];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
+       private static void task2() {
+        System.out.println("Задача 2");
+        int[] payments = generateRandomArray();
+        System.out.println(Arrays.toString(payments));
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < payments.length; i++) {
+            if (payments[i] > max) {
+                max = payments[i];
             }
-            return arr;
+            if (payments[i] < min) {
+                min = payments[i];
+            }
+        }
+        if (min != Integer.MAX_VALUE && max != Integer.MAX_VALUE) {
+            System.out.println("Минимальная сумма трат за день составила" + min + "рублей. Максимальная сумма трат за день составила" + max + "рублей.");
         }
     }
+
+    // Объявить метод «сгенерироватьМассив»
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+}
+
+
         
 
 
